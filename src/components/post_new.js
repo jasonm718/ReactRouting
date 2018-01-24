3aa6a80c92f368874215a059e31e80eb4c2ddd93
Jason Morales <jasonm718@gmail.com>
2018-01-24 15:40:20 -0600

    styling aded and post route was created

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -1,31 +1,48 @@
 import React, { Component } from 'react';
 import {Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 class PostNew extends Component {
   renderField(field){
+    const { meta: {touched, error} } = field;
+    const className= `form-group ${touched && error ? 'has-danger' : ''}`;
+
     return(
-      <div className="form-group">
+      <div className={className}>
         <label>{field.label}</label>
         <input
         className="form-control"
           type="text"
           {...field.input}
         />
+        <div className="text-help">
+          { touched ? error : ''}
+        </div>
       </div>
     );
   }
 
+  onSubmit(values) {
+    // this === component
+    //console.log(values);
+    this.props.createPost(values);
+  }
+
   render() {
+    const { handleSubmit } = this.props;
+
     return(
-      <form>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <Field
-          label="Title"
+          label="Title For Post"
           name="title"
           component={this.renderField}
         />
         <Field
-          label="Tags"
-          name="tags"
+          label="Categories"
+          name="categories"
           component={this.renderField}
         />
         <Field
@@ -33,11 +50,36 @@ class PostNew extends Component {
           name="content"
           component={this.renderField}
         />
+        <button type="submit" className="btn btn-primary">Submit</button>
+        <Link to="/" className="btn btn-danger">Cancel</Link>
       </form>
     );
   }
 }
 
+function validate(values) {
+  //console.log(values) -> {title: 'asd', categories: 'adsad', content: 'asdasd'}
+  const errors = {};
+
+  //Validate the inputs from 'values'
+  if(!values.title || values.title.length <3){
+    errors.title = 'Enter a title!';
+  }
+  if(!values.categories) {
+    errors.categories = 'Enter some categories';
+  }
+  if(!values.content) {
+    errors.content = 'Enter some content please';
+  }
+
+  // If errors is empty, the fom is fine to submit
+  // If errors has *any* properties, redux form assumes form is invalid
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: 'PostsNewForm'
-})(PostNew);
+})(
+  connect(null,{ createPost })(PostNew)
+);
