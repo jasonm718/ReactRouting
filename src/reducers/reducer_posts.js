5428c134f588d86dc68a45bc901f3129b3cebc07
Jason Morales <jasonm718@gmail.com>
2018-01-25 12:45:07 -0600

    Added delete function

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,11 +1,18 @@
 import _ from 'lodash';
-import { FETCH_POSTS } from '../actions';
+import { FETCH_POST, FETCH_POSTS } from '../actions';
 
 export default function (state = {}, action) {
   switch (action.type) {
+
+    case FETCH_POST:
+      //const post = action.payload.data;
+      //const newState = {...state};
+      //newState[post.id] = post;
+      //return newState;
+      return {...state, [action.payload.data.id]: action.payload.data };
     case FETCH_POSTS:
       //console.log(action.payload.data); // [pst1. pst2]
-      // {4: post}
+      // {4: post}osts/193562
       return _.mapKeys(action.payload.data, 'id');
     default:
       return state;
